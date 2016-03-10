07bb9f91981c4eb224668f55c2106df4c08c0096
Sean Porter <portertech@gmail.com>
2016-03-10 10:54:57 -0800

    bundle gem sensu-redis

---
+++ lib/sensu/redis.rb
@@ -0,0 +1,7 @@
+require "sensu/redis/version"
+
+module Sensu
+  module Redis
+    # Your code goes here...
+  end
+end
---
+++ lib/sensu/redis/version.rb
@@ -0,0 +1,5 @@
+module Sensu
+  module Redis
+    VERSION = "0.1.0"
+  end
+end
